d163b95534fc5a3ed9c5c552108cd839d2252a3d
Vladislav Bodrevskiy <anpathfinder@mail.ru>
2025-02-04 15:55:12 +0300

    feat: project structure

---
+++ src/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+    CHAT_ID: str
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
